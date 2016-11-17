3b092c2eeff4391d2def92c249a7f267e83f07f8
Stephanie Picard <Stephanie Picard>
2016-11-17 15:01:33 -0500

    feat:  original coding for player and enemy entities and functionality, lots of bugs!!!

--- js/app.js
+++ js/app.js
@@ -9,61 +9,87 @@ var Enemy = function(x,y,speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x += this.speed*dt
-    
+    this.x += this.speed*dt;
+    checkCollision(x,y);
     if (this.x >= 505) {
         x=0;
-    }
+    };
 };
-
-checkCollision(this);
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+//using the axis-aligned bounding box algorithm modified
+//with the actual start locations and width/heights for
+//the bug(enemy) and cat(player) within their image files
+Enemy.prototype.checkCollision = function(x,y) {
+    
+    if (enemy.x < player.x + 90 &&
+        enemy.x + 101 > player.x + 10 &&
+        enemy.y + 70 < player.y + 140 &&
+        enemy.y + 150 > player.y + 60) {
+        alert("Game Over");
+        document.location.reload();
+    };
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (x,y,){
+var Player = function (x, y, stepsize) {
     this.sprite = 'images/char-cat-girl.png';
-    this.x=x
-    this.y=y
- //need step size given interpret?
+    this.x=x;
+    this.y=y;
+    this.stepsize = stepsize;
+ };
 
-Player.prototype.update = function(x,y) {
-    if (y<83) {
-        player.reset();
-    }
+Player.prototype.update = function() {
+    stepsize = 20;
+    handleInput(allowedKeys, stepsize);
+    if (y<3) {
+        alert("You Won!");
+        document.location.reload();
+    };
+};
+
 Player.prototype.render = function () {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
-}
+};
 
-//changes the x and y draw start coordinates of the player based
-//on the input key while also not allowing coordinate change if
-//off canvas
-Player.prototype.handleInput = function (allowedKeys) {
-    if (allowedKeys===37 and x>101) {
-        x=x-101;
-    } else if (allowedKeys===38 and y<415) {
-        y=y+83;
-    } else if (allowedKeys===39 and x<303) {
-        x=x+101;
-    } else if (allowedKeys===40 and y>83) {
-        y=y-83;
+//
+Player.prototype.handleInput = function (allowedKeys, stepsize) {
+    if (allowedKeys ='left' && x>80) {
+        x -= stepsize;
     }
-}
+    if (allowedKeys ='up' && y>80) {
+        y -= stepsize;
+    } 
+    if (allowedKeys = 'right' && x<425) {
+        x += stepsize;
+    } 
+    if (allowedKeys = 'down' && y<418) {
+        y += stepsize;
+    }
+};
 
-//congratulates the winner and re-initializes game
-Player.prototype.reset = function () {
-//ToDO    
-}
+var enemy = new Enemy(0, Math.random()*165+83, Math.random()*1000);
 
-enemy = new Enemy(0, Math.random()// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [enemy];
 
+/**var multEnemies = function() {
+    var enemycount = 6;
+    for (i=0, i < enemycount + 1, i++) {
+        enemy();
+        allEnemies.protoptype.push(enemy);
+    };
+}; 
+**/
+
+
+var player = new Player(202,415);
 
 
 // This listens for key presses and sends the keys to your
