afa0ac424129481964b53ef15717a326d6aad342
Stephanie Picard <Stephanie Picard>
2016-12-02 15:00:37 -0500

    feat:  add random bug generator, collision detector, game resets

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-/// Enemies our player must avoid
+/** Enemy class constructor
+    Creates a new bug starting to the left of the game board
+    with random y placement centering within the block rows
+    and with a random speed
+**/
+
 var Enemy = function(x,y,speed) {
-    this.x =x;
-    this.y = y;
-    this.speed = speed;
+    this.x = -101;
+    this.y = Math.floor((Math.random() * 3) + 1) * 70;
+    this.speed = Math.floor((Math.random() * 3) + 1) * 100;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -10,10 +15,10 @@ var Enemy = function(x,y,speed) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     this.x += this.speed*dt;
-//    checkCollision(x,y);
     if (this.x >= 505) {
-        x=0;
+        this.x = 0;
     };
+    this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,27 +30,24 @@ Enemy.prototype.render = function() {
 //using the axis-aligned bounding box algorithm modified
 //with the actual start locations and width/heights for
 //the bug(enemy) and cat(player) within their image files
-/**Enemy.prototype.checkCollision = function(x,y) {
-    
-    if (enemy.x < player.x + 90 &&
-        enemy.x + 101 > player.x + 10 &&
-        enemy.y + 70 < player.y + 140 &&
-        enemy.y + 150 > player.y + 60) {
-        alert("Game Over");
+Enemy.prototype.checkCollision = function() {
+    if (this.x < player.x + 50 &&
+        this.x + 50 > player.x &&
+        this.y < player.y + 80 &&
+        this.y + 80 > player.y) {
+        alert("Oh no, collision!");
         document.location.reload();
     };
-}; **/
+}; 
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function () {
     this.sprite = 'images/char-cat-girl.png';
-    this.x=202;
-    this.y=415;
+    this.x = 202;
+    this.y = 415;
  };
-
-//stepsize = 20;
 
 Player.prototype.update = function() {
     if (this.y<2) {
@@ -67,36 +69,27 @@ Player.prototype.render = function () {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
 };
 
-//
+
 Player.prototype.handleInput = function (direction) {
-    if (direction ==='left'/** && x>80 **/) {
-        this.x -= 10;
+    if (direction === 'left') {
+        this.x -= 20;
     }
-    if (direction ==='up' /*&& y>80**/) {
-        this.y -= 10;
+    if (direction === 'up') {
+        this.y -= 20;
     }
-    if (direction === 'right'/* && x<425*/) {
-        this.x += 10;
+    if (direction === 'right') {
+        this.x += 20;
     } 
-    if (direction === 'down'/** && y<418 **/) {
-        this.y += 10;
+    if (direction === 'down') {
+        this.y += 20;
     }
 };
 
-var enemy = new Enemy(0, Math.random()*165+83, Math.random()*1000);
-
 var allEnemies = [];
 
-allEnemies[0] = enemy;
-allEnemies[1] = enemy;
-allEnemies[2] = enemy;
-
-/** var addEnemies = function () {
-    var enemycount = 6;
-    for (var i=0; i < enemycount; i++) {
-        allEnemies.push(enemy);
-    };
-}; **/
+for (var i = 0; i<3; i++) {
+    allEnemies[i] = new Enemy();
+}
 
 var player = new Player();
 
